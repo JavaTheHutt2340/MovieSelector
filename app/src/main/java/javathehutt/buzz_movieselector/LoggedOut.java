4a8723425d8c4e7f1cc0504e99b5823664252fd6
Saqib <saqibm128@gmail.com>
2016-02-02 18:20:20 -0500

    makes foundation for logout

---
+++ app/src/main/java/javathehutt/buzz_movieselector/LoggedOut.java
@@ -0,0 +1,13 @@
+package javathehutt.buzz_movieselector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoggedOut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_out);
+    }
+}
