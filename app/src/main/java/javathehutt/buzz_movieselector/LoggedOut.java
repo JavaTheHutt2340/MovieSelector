d1cd3cf2d4426a498b75b53d30ebb3c4089bbe27
Saqib <saqibm128@gmail.com>
2016-02-03 18:40:02 -0500

    trying a bug fix

--- app/src/main/java/javathehutt/buzz_movieselector/LoggedOut.java
+++ app/src/main/java/javathehutt/buzz_movieselector/LoggedOut.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.R;
 
 public class LoggedOut extends AppCompatActivity {
 
