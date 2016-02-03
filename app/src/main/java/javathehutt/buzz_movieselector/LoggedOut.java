b67c39615036683125a4a8522a821aa215dd5723
Saqib <saqibm128@gmail.com>
2016-02-03 00:38:22 -0500

    trying to add functionality to return home button

--- app/src/main/java/javathehutt/buzz_movieselector/LoggedOut.java
+++ app/src/main/java/javathehutt/buzz_movieselector/LoggedOut.java
@@ -1,7 +1,9 @@
 package javathehutt.buzz_movieselector;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoggedOut extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoggedOut extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_out);
+    }
+    public void returnHome(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
