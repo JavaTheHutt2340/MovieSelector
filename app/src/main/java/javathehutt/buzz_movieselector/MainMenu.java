a775937b25f6107bf3059d72bcd140a92d9082eb
Saqib <saqibm128@gmail.com>
2016-02-06 17:30:19 -0500

    created crude mechanism for log out TODO:allow for better transfer of info

--- app/src/main/java/javathehutt/buzz_movieselector/MainMenu.java
+++ app/src/main/java/javathehutt/buzz_movieselector/MainMenu.java
@@ -5,14 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import javathehutt.buzz_movieselector.model.AuthenticationManager;
+import javathehutt.buzz_movieselector.model.User;
+import javathehutt.buzz_movieselector.model.UserMapManager;
+
 public class MainMenu extends AppCompatActivity {
+
+    private User u;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        AuthenticationManager a = new UserMapManager();
+        u = a.lastLogIn();
     }
     public void logOut(View v) {
+        u.logout();
         Intent i = new Intent(this, LoggedOut.class);
         startActivity(i);
     }
