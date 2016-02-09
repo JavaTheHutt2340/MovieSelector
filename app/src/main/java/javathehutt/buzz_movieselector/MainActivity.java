fb90c4e8fbc3fd30a9c1460c5246007751ea3dcd
Frank Marzen <fmarzen@gmail.com>
2016-02-08 20:51:19 -0500

    Set conventions for layout files (relative layout, no content.xml files) and made other small changes. Fixed back button going to unintended activity

--- app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
+++ app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
@@ -10,26 +10,37 @@ import android.widget.Button;
 import javathehutt.buzz_movieselector.model.RegUser;
 import javathehutt.buzz_movieselector.model.UserManager;
 import javathehutt.buzz_movieselector.model.UserMapManager;
-import javathehutt.buzz_movieselector.model.welcome_screen_activity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button loginButton, registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome_screen);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        registerButton = (Button) findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(this);
 
         UserManager userManager = new UserMapManager();
         userManager.addUser(new RegUser("user", "pass"));
-        Intent welcome = new Intent(this, welcome_screen_activity.class);
-        startActivity(welcome);
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
-            case R.id.registerButton: // place buttton id here
-                //TODO place action to occur when button pressed here
+            case R.id.loginButton:
+                Intent login = new Intent(this, loginActivity.class);
+                startActivity(login);
+                //TODO
+                //finish() if logged in
+                //else do not finish
+                break;
+            case R.id.registerButton:
+
                 break;
         }
     }
