a36f3c73aedcbc77002afcfba44026d027871023
jgibbygibs <jgibson@gatech.edu>
2016-02-13 11:44:11 -0500

    conformed to coventions and fixed a few redundant lines of code

--- app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
+++ app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
@@ -11,40 +11,20 @@ import javathehutt.buzz_movieselector.model.RegUser;
 import javathehutt.buzz_movieselector.model.UserManager;
 import javathehutt.buzz_movieselector.model.UserMapManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    Button loginButton, registerButton;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
-        loginButton = (Button) findViewById(R.id.loginButton);
-        registerButton = (Button) findViewById(R.id.registerButton);
-
-        loginButton.setOnClickListener(this);
-        registerButton.setOnClickListener(this);
-
         UserManager userManager = new UserMapManager();
         userManager.addUser(new RegUser("user", "pass"));
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.loginButton:
-                Intent login = new Intent(this, loginActivity.class);
-                startActivity(login);
-                //TODO
-                //finish() if logged in
-                //else do not finish
-                break;
-            case R.id.registerButton:
-                Intent register = new Intent(this, RegisterActivity.class);
-                startActivity(register);
-                break;
-        }
+    public void registerButtonClick(View v) {
+        Intent register = new Intent(this, RegisterActivity.class);
+        startActivity(register);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
