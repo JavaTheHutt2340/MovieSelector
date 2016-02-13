ed7911abcbb8b5f7c6cb5588cefbc2ab6422d03c
sali45 <sali45@gatech.edu>
2016-02-13 01:48:38 -0500

    Pushing to master all changes ya boi Saqib.

--- app/src/main/java/javathehutt/buzz_movieselector/loginActivity.java
+++ app/src/main/java/javathehutt/buzz_movieselector/loginActivity.java
@@ -3,16 +3,11 @@ package javathehutt.buzz_movieselector;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import javathehutt.buzz_movieselector.model.User;
 import javathehutt.buzz_movieselector.model.UserManager;
 import javathehutt.buzz_movieselector.model.UserMapManager;
 
@@ -41,8 +36,8 @@ public class loginActivity extends AppCompatActivity {
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
-                setResult(1);
-                finish();
+                Intent intent = new Intent(this, MainMenu.class);
+                startActivity(intent);
             } else {
                 Context context = getApplicationContext();
                 CharSequence text = "Invalid Password!";
