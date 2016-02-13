465b92e9bea2e10493d66fe4f24f5a967122ef8b
sali45 <sali45@gatech.edu>
2016-02-13 16:20:27 -0500

    Fixed register crashing.

--- app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
+++ app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
@@ -3,49 +3,45 @@ package javathehutt.buzz_movieselector;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import javathehutt.buzz_movieselector.model.RegUser;
 import javathehutt.buzz_movieselector.model.UserManager;
 import javathehutt.buzz_movieselector.model.UserMapManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    Button loginButton, registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
-        loginButton = (Button) findViewById(R.id.loginButton);
-        registerButton = (Button) findViewById(R.id.registerButton);
-
-        loginButton.setOnClickListener(this);
-        registerButton.setOnClickListener(this);
-
         UserManager userManager = new UserMapManager();
         userManager.addUser(new RegUser("user", "pass"));
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.loginButton:
-                Intent login = new Intent(this, loginActivity.class);
-                startActivity(login);
-                //TODO
-                //finish() if logged in
-                //else do not finish
-                break;
-            case R.id.registerButton:
-                Intent register = new Intent(this, RegisterActivity.class);
-                startActivity(register);
-                break;
-        }
+    public void registerButtonClick(View v) {
+        Intent register = new Intent(this, RegisterActivity.class);
+        startActivity(register);
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.loginButton:
+//                Intent login = new Intent(this, loginActivity.class);
+//                startActivity(login);
+//                //TODO
+//                //finish() if logged in
+//                //else do not finish
+//                break;
+//            case R.id.registerButton:
+//                Intent register = new Intent(this, RegisterActivity.class);
+//                startActivity(register);
+//                break;
+//        }
+//    }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == 1) {
--- app/src/main/java/javathehutt/buzz_movieselector/loginActivity.java
+++ app/src/main/java/javathehutt/buzz_movieselector/loginActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import javathehutt.buzz_movieselector.model.UserManager;
@@ -13,7 +12,6 @@ import javathehutt.buzz_movieselector.model.UserMapManager;
 
 public class loginActivity extends AppCompatActivity {
 
-    Button userLoginButton, cancelButton;
     EditText etUsername, etPassword;
 
 
