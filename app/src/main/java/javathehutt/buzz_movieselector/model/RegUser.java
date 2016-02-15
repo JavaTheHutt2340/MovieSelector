771a52ccb0ea913cc708d08a00fb20d932c929f6
jgibbygibs <jgibson@gatech.edu>
2016-02-15 13:14:37 -0500

    added javadocs

--- app/src/main/java/javathehutt/buzz_movieselector/model/RegUser.java
+++ app/src/main/java/javathehutt/buzz_movieselector/model/RegUser.java
@@ -8,23 +8,42 @@ public class RegUser extends User{
     private int failedAttempts;
     private final int attemptsAllowed = 5;
 
+    /*
+    * constructor for the User class
+    * @param username the users username
+    * @param password the users password
+     */
     public RegUser(String username, String password) {
         super(username, password);
         locked = false;
     }
 
+    /*
+    * used to determined if the account is locked
+    * @return locked ture if the user is locked false otherwise
+     */
     public boolean getLockStatus() {
         return locked;
     }
 
+    /*
+    * locks the user
+     */
     public void lock() {
         locked = true;
     }
 
+    /*
+    * unlocks the user account
+     */
     public void unlock() {
         locked = false;
     }
 
+    /*
+    * @param password the password the person is using to login
+    * @return true if the password matches the stored password
+     */
     @Override
     public boolean logIn(String password) {
         if (locked) {
--- app/src/main/java/javathehutt/buzz_movieselector/model/User.java
+++ app/src/main/java/javathehutt/buzz_movieselector/model/User.java
@@ -9,19 +9,33 @@ public abstract class User {
     private boolean loggedIn;
 
 
-
+    /*
+    * @param username the users username
+    * @param password the users password
+     */
     public User(String username, String password) {
         this.username = username;
         this.password = password;
         loggedIn = false;
     }
+    /*
+    * used to tell if the user is an admin user
+    * @return true if the user is an admin user
+     */
     public boolean isAdmin() {
         return this instanceof AdminUser;
     }
+
+    /*
+    * sets the users logged in status to false
+     */
     public void logout() {
         loggedIn = false;
     }
 
+    /*
+    *
+     */
     public String getUsername() {
         return username;
     }
