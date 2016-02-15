fb186da02ee63b0ace8150a86d5a196026aa5e8c
Saqib <saqibm128@gmail.com>
2016-02-15 13:16:06 -0500

    adds javadocs to UserMapManager and LoggedOut

--- app/src/main/java/javathehutt/buzz_movieselector/LoggedOut.java
+++ app/src/main/java/javathehutt/buzz_movieselector/LoggedOut.java
@@ -1,3 +1,9 @@
+/**
+ * Java controller for LoggedOut screen
+ * @author Mohammed Saqib
+ * @version 1.0
+ * @date 02/15/16
+ */
 package javathehutt.buzz_movieselector;
 
 import android.content.Intent;
@@ -6,16 +12,30 @@ import android.os.Bundle;
 import android.view.View;
 
 public class LoggedOut extends AppCompatActivity {
-
+    /**
+     * creates and sets the screen
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_out);
     }
+
+    /**
+     * Method for button "Return Home"
+     * Returns user to Home screen.
+     * @param v current view
+     */
     public void returnHome(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    /**
+     * Method for button
+     * @param v current view
+     */
     public void returnLogIn(View v) {
         Intent intent = new Intent(this, loginActivity.class);
         startActivity(intent);
