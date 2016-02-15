12bdfee6d0813f3b40e477b5be9642cd23a9cfbe
Saqib <saqibm128@gmail.com>
2016-02-15 09:53:23 -0500

    places @Override on appropriate methods

--- app/src/main/java/javathehutt/buzz_movieselector/model/AdminUser.java
+++ app/src/main/java/javathehutt/buzz_movieselector/model/AdminUser.java
@@ -7,7 +7,7 @@ public class AdminUser extends User {
     public AdminUser(String username, String password) {
         super(username, password);
     }
-
+    @Override
     public boolean getLockStatus() {
         return false;
     }
