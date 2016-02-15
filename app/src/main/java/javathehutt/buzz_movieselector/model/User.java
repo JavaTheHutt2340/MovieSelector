39176563323bf9bdf611c6dc453a993f7018254b
jgibbygibs <jgibson@gatech.edu>
2016-02-15 13:46:08 -0500

    fixed hashcode method

--- app/src/main/java/javathehutt/buzz_movieselector/model/User.java
+++ app/src/main/java/javathehutt/buzz_movieselector/model/User.java
@@ -102,8 +102,8 @@ public abstract class User {
      */
     public int hashCode() {
         int result = 0;
-        result *= 17 * password.hashCode();
-        result *= 21 * username.hashCode();
+        result += 17 * password.hashCode();
+        result += 21 * username.hashCode();
         return result;
     }
 
