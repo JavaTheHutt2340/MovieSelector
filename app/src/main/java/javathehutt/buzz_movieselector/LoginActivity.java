17374661de8e782bae8af245ee150431ad9a1cba
Saqib <saqibm128@gmail.com>
2016-02-15 16:05:53 -0500

    added back LogInActivity correctly

---
+++ app/src/main/java/javathehutt/buzz_movieselector/LoginActivity.java
@@ -0,0 +1,58 @@
+package javathehutt.buzz_movieselector;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import javathehutt.buzz_movieselector.model.UserManager;
+import javathehutt.buzz_movieselector.model.UserMapManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+    }
+
+    public void userLoginButtonClick(View v) {
+        UserManager userManager = new UserMapManager();
+        if (userManager.isInSystem(etUsername.getText().toString().toLowerCase())) {
+            if (userManager.handleLogInRequest(etUsername.getText().toString().toLowerCase(), etPassword.getText().toString())) {
+                Context context = getApplicationContext();
+                CharSequence text = "Log In Success!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                Intent intent = new Intent(this, MainMenu.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Context context = getApplicationContext();
+                CharSequence text = "Invalid Password!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "User Does Not Exist!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+
+    public void cancelButtonClick(View v) {
+        finish();
+    }
+}
