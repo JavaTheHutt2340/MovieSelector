66b0f8af5b43eae0692d71b23110e56929afb822
Saqib <saqibm128@gmail.com>
2016-02-16 22:19:43 -0500

    implemented HttpRequest bufferedReader, and created primitive way to create movies from it

--- app/src/main/java/javathehutt/buzz_movieselector/movie/Movie.java
+++ app/src/main/java/javathehutt/buzz_movieselector/movie/Movie.java
@@ -4,5 +4,34 @@ package javathehutt.buzz_movieselector.movie;
  * Created by Mohammed on 2/16/2016.
  */
 public class Movie {
+    private String name;
+    private int year;
+    private String mpaa_rating;
+    private int runTime;
+    private String criticsConsensus;
+    private String criticsRating;
+    private int criticsScore;
+    private String audienceRating;
+    private int audienceScore;
 
+    public Movie(){}
+
+    public Movie(String name, int year){
+        this.name = name;
+        this.year = year;
+    }
+
+    public Movie(String name, int year, String mpaa_rating, int runtime,
+                 String criticsConsensus, String criticsRating, int criticsScore, String audienceRating,
+                 int audienceScore) {
+        this.name = name;
+        this.year = year;
+        this.mpaa_rating = mpaa_rating;
+        this.runTime = runtime;
+        this.criticsConsensus = criticsConsensus;
+        this.criticsRating = criticsRating;
+        this.criticsScore = criticsScore;
+        this.audienceRating = audienceRating;
+        this.audienceScore = audienceScore;
+    }
 }
--- app/src/main/java/javathehutt/buzz_movieselector/movie/RottenTomatoesJSON.java
+++ app/src/main/java/javathehutt/buzz_movieselector/movie/RottenTomatoesJSON.java
@@ -1,7 +1,10 @@
 package javathehutt.buzz_movieselector.movie;
 
 import com.github.kevinsawicki.http.HttpRequest;
-import com.github.kevinsawicki.http.HttpRequest.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.Scanner;
 
 /**
  * Created by Mohammed on 2/16/2016.
@@ -31,8 +34,36 @@ public class RottenTomatoesJSON implements RottenTomatoes {
     public HttpRequest makeUrlByName(String name){
         //http://api.rottentomatoes.com/api/public/v1.0/movies.json
         //yedukp76ffytfuy24zsqk7f5
-        return HttpRequest.get("http://api.rottentomatoes.com/api/public/v1.0/movies.json", true,
-                "apikey", key, "q", name);
+        try {
+            return HttpRequest.get("http://api.rottentomatoes.com/api/public/v1.0/movies.json", true,
+                    "apikey", key, "q", name);
+        } catch (HttpRequest.HttpRequestException e) {
+            return null;
+        }
     }
-    public
+    private BufferedReader extractReader(HttpRequest h) {
+        try {
+            if (h.ok()) {
+                return h.bufferedReader("\",:{}[]");
+            }
+            return null;
+        } catch (HttpRequest.HttpRequestException e) {
+            return null;
+        }
+    }
+    public Movie makeMovie(BufferedReader b) {
+        Scanner scan = new Scanner(b);
+        String title = "";
+        int year = 0;
+        while(scan.hasNext()) {
+            String toProcess = scan.next();
+            if(toProcess.equals("title")) {
+                title = scan.next();
+            }
+            if(toProcess.equals("year")) {
+                year = new Integer(scan.next());
+            }
+        }
+        return new Movie(title, year);
+    }
 }
