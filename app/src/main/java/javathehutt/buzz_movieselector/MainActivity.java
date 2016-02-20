10f834d4467c98de437e8d3ce1737acf55a35995
Daniel Kane <dpk2029@gmail.com>
2016-02-20 15:32:20 -0500

    search

--- app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
+++ app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.SearchView;
 
 import javathehutt.buzz_movieselector.movie.RottenTomatoes;
 import javathehutt.buzz_movieselector.movie.RottenTomatoesJSON;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        SearchView searchBar = (SearchView) findViewById(R.id.searchView);
+        searchBar.setQueryHint("Search Movie");
         /*RottenTomatoesJSON r = new RottenTomatoesJSON(MainActivity.this);
         r.newMovieReleases(10);*/ //TODO this is how to use the class
     }
@@ -45,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(login, 1);
     }
 
+    public void searchMovies(View v){
+        SearchView searchBar = (SearchView) findViewById(R.id.searchView);
+        String searchText = searchBar.getQuery().toString();
+        RottenTomatoesJSON RTJSON = new RottenTomatoesJSON(this);
+        RTJSON.searchMovieByName(searchText,12);
+    }
 }
 
 
