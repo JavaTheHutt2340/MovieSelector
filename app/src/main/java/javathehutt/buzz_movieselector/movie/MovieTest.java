818622c1cff988b70042b997fd4cc01361f1fdd4
Saqib <saqibm128@gmail.com>
2016-02-19 19:39:33 -0500

    added junits

---
+++ app/src/main/java/javathehutt/buzz_movieselector/movie/MovieTest.java
@@ -0,0 +1,11 @@
+package javathehutt.buzz_movieselector.movie;
+
+/**
+ * Created by Mohammed on 2/19/2016.
+ */
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.AndroidJUnitRunner;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MovieTest {
+}
