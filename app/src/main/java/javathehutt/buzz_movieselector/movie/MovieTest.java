7befa53671f2ea94e0ffea3f495be88e0ffe3057
Saqib <saqibm128@gmail.com>
2016-02-19 19:46:25 -0500

    will fix jUnits later

--- app/src/main/java/javathehutt/buzz_movieselector/movie/MovieTest.java
+++ app/src/main/java/javathehutt/buzz_movieselector/movie/MovieTest.java
@@ -3,8 +3,8 @@ package javathehutt.buzz_movieselector.movie;
 /**
  * Created by Mohammed on 2/19/2016.
  */
-import android.support.test.runner.AndroidJUnit4;
-import android.support.test.runner.AndroidJUnitRunner;
+import android.support.test.runner.*;
+import android.support.test.runner.*;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class MovieTest {
