34fa880200ee251034a4b2d17666125842b94b80
Saqib <saqibm128@gmail.com>
2016-02-27 18:50:53 -0500

    added Facebook Login button

--- app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
+++ app/src/main/java/javathehutt/buzz_movieselector/MainActivity.java
@@ -2,11 +2,21 @@ package javathehutt.buzz_movieselector;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.SearchView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import android.content.pm.Signature;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 import javathehutt.buzz_movieselector.movie.RottenTomatoes;
 import javathehutt.buzz_movieselector.movie.RottenTomatoesJSON;
@@ -16,11 +26,41 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_welcome_screen);
+
+        //This code will create Facebook hash for android development
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "javathehutt.buzz_movieselector",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KeyHash:", "++++++++++++++++++++++++++++++++++++++" + Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("KeyHash:", "++++++++++++++++++++++++++++++++++++++" + e.toString());
+
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("KeyHash:", "++++++++++++++++++++++++++++++++++++++" + e.toString());
+        }
 
         /*RottenTomatoesJSON r = new RottenTomatoesJSON(MainActivity.this);
         r.newMovieReleases(10);*/ //TODO this is how to use the class
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        // Track metrics for app using Facebook tools
+        AppEventsLogger.activateApp(this);
+    }
+
+
 
     /**
      * Method called when User
