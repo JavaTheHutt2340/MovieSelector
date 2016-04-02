ccabc75c77e6ab903404a6612c4116cda87ad6cf
jgibbygibs <jgibson@gatech.edu>
2016-04-02 16:11:32 -0400

    fixed uncompile issue

---
+++ app/src/androidTest/java/javathehutt/buzz_movieselector/JasonTest.java
@@ -0,0 +1,35 @@
+package javathehutt.buzz_movieselector;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by JasonGibson on 4/2/16.
+ */
+public class JasonTest extends ApplicationTestCase<Application> {
+    /**
+     * constructor
+     */
+    public JasonTest() {
+        super(Application.class);
+    }
+
+    @Before
+    public void setup() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void add() {
+
+    }
+}
